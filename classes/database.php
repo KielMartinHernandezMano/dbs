95b8ec187b5b83bcf3673a2da2aadb80498793bc
KielMartinHernandezMano <manokh@students.nu-lipa.edu.ph>
2025-05-20 15:07:22 +0800

    dbs

---
+++ classes/database.php
@@ -0,0 +1,54 @@
+<?php
+ 
+class database{
+ 
+    function opencon(): PDO{
+        return new PDO(
+            dsn: 'mysql:host=localhost;
+            dbname=dbs_app',
+            username: 'root',
+            password: ''
+        );
+    }
+
+    function signupUser($firstname,$lastname,$username, $email,$password){
+        $con = $this->opencon();
+        try{
+            $con-> beginTransaction();
+             $stmt=$con->prepare("INSERT INTO Admin(
+            admin_FN,
+            admin_LN,
+            admin_username,
+            admin_email,
+            admin_password)VALUES(?,?,?,?,?)");
+            $stmt->execute([$firstname,$lastname,$username, $email, $password]);
+            $userID = $con->lastInsertId();
+            $con-> commit();
+            return $userID;
+
+        } catch (PDOException $E){
+            $con-> rollBack();
+            return false;
+
+        }
+    }
+
+    function isUsernameExists($username){
+        $con = $this->opencon();
+        $stmt = $con->prepare("SELECT * FROM Admin WHERE admin_username = ?");
+        $stmt->execute([$username]);
+        $count = $stmt->fetchColumn();
+ 
+        return $count > 0;
+    }
+    function isEmailExists($email){
+        $con = $this->opencon();
+        $stmt = $con->prepare("SELECT * FROM Admin WHERE admin_email = ?");
+        $stmt->execute([$email]);
+        $count = $stmt->fetchColumn();
+ 
+        return $count > 0;
+}
+}
+ 
+ 
